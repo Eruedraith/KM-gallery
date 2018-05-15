e228de4af123bb8fa97c77d459c9d43c29d2259c
Eruedraith <eruedraith777@gmail.com>
2018-05-15 14:21:55 -0400

    added browser sync and hubspot layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browserSync').create();
 
+// compile SCSS & reload browser
 gulp.task('sass', function() {
 	return gulp.src('./scss/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.reload{stream:true});
 });
+
 
 gulp.task('watch', function() {
 	gulp.watch('./scss/*.scss', ['sass']);
+	gulp.watch('.**/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('serve', function() {
+
+	browserSync.init( {
+		server: {
+			baseDir: './'
+		}
+	});
+});
+
+// set defaults
+gulp.task('default', ['sass', 'watch', 'serve']);
