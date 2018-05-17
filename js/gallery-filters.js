132234051614e24ac4ac1007d337e327a1f40521
Eruedraith <eruedraith777@gmail.com>
2018-05-17 12:49:22 -0400

    added jquery and custom js

--- js/gallery-filters.js
+++ js/gallery-filters.js
@@ -1,8 +1,6 @@
-/** open and close gallery filter cards ***/
+/* open and close gallery filter cards */
 $(document).ready(function() {
 	$('.gallery__filter-card--dropdown').on('click', function() {
-		$('.gallery__filter-card--body').toggle('slow'):
+		$('.gallery__filter-card--body').toggle('slow');
 	});
 });
-
-console.log('test');
