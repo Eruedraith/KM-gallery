4a49726db27336e0da2ed6cab708366eea5d525c
Eruedraith <eruedraith777@gmail.com>
2018-05-18 17:12:15 -0400

    added hubl and filter jQuery

--- js/gallery-filters.js
+++ js/gallery-filters.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-	$('.gallery__filter-card--header').on("click", function(event) {
-		$(event.target).closest(".gallery__filter-card").find('.gallery__filter-card--body').toggle('fast');
+	// accordion sidebar 
+	$('.gallery__filter-card--header').on("click", function(e) {
+		$(e.target).closest(".gallery__filter-card").find('.gallery__filter-card--body').slideToggle('400');
 	});
+
+	// filters
+	$('.form-check > input').click(function() {
+		var value = $(this).attr('data-filter');
+		$('.gallery__img--card').not('.'+value).hide('3000');
+		$('.gallery__img--card').filter('.'+value).show('3000');
+	});
+
 });
